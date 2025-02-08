dbe20fd44ed99f161c65ca4bb1d571c160c4f927
hacker1024 <hacker1024@users.sourceforge.net>
2025-02-08 20:20:33 +1100

    mimalloc: Use `tag` with `fetchFromGitHub`
    Co-authored-by: kirillrdy <kirillrdy@gmail.com>

--- pkgs/by-name/mi/mimalloc/package.nix
+++ pkgs/by-name/mi/mimalloc/package.nix
@@ -18,7 +18,7 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "mimalloc";
-    rev = "v${finalAttrs.version}";
+    tag = "v${finalAttrs.version}";
     sha256 = "sha256-RjFMLInulyynrmyAg6Km/u9HGfcYeerkou0gQwCdhsA=";
   };
 
