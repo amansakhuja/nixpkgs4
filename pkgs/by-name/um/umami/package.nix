453c3be35adc9161a4e1c9247a22a6f4bfdd6fb1
Diogo Correia <me@diogotc.com>
2025-02-10 13:39:09 +0000

    nixos/tests/umami: init

--- pkgs/by-name/um/umami/package.nix
+++ pkgs/by-name/um/umami/package.nix
@@ -4,6 +4,7 @@
   fetchFromGitHub,
   fetchurl,
   makeWrapper,
+  nixosTests,
   nodejs,
   pnpm_10,
   prisma-engines,
@@ -173,6 +174,9 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   '';
 
   passthru = {
+    tests = {
+      inherit (nixosTests) umami;
+    };
     inherit
       sources
       geocities
